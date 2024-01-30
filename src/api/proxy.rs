7393c4c60db2537346ca252a65f25aa8ed20c965
Sam Ulrich <40002776+sam-ulrich1@users.noreply.github.com>
2024-01-30 11:59:44 -0600

    Cargo fmt

--- src/api/proxy.rs
+++ src/api/proxy.rs
@@ -96,7 +96,11 @@ async fn connected(
     } else {
         &ctx.commands[0]
     };
-    tracing::info!("starting {} in {}", command[0], ctx.cwd.as_str().replace("file://", ""));
+    tracing::info!(
+        "starting {} in {}",
+        command[0],
+        ctx.cwd.as_str().replace("file://", "")
+    );
     let mut server = Command::new(&command[0])
         .args(&command[1..])
         .current_dir(ctx.cwd.as_str().replace("file://", ""))
